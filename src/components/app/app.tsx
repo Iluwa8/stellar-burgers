5d9d9a938809f8c81a8266a67d02f73702914ae8
Ilya Sokolov <iluwa8@yandex.ru>
2026-05-14 17:12:42 +0300

    routes ready

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,33 @@
-import { ConstructorPage } from '@pages';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import '../../index.css';
 import styles from './app.module.css';
+import { useSelector } from 'react-redux';
 
 import { AppHeader } from '@components';
 import { Preloader } from '@ui';
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  NotFound404
+} from '@pages';
+import { Modal, OrderInfo, IngredientDetails } from '@components';
 
 const App = () => {
-  /** TODO: взять переменные из стора */
+  // const { isLoading, ingreedients, error } = useSelector(
+  //   (state) => state.ingredients
+  // );
+  const location = useLocation();
+  const navigate = useNavigate();
+  const background = location.state?.background;
+
+  const handleModalClose = () => navigate(-1);
+
   const isIngredientsLoading = false;
   const ingredients = [];
   const error = null;
@@ -14,6 +35,7 @@ const App = () => {
   return (
     <div className={styles.app}>
       <AppHeader />
+
       {isIngredientsLoading ? (
         <Preloader />
       ) : error ? (
@@ -21,10 +43,51 @@ const App = () => {
           {error}
         </div>
       ) : ingredients.length > 0 ? (
-        <ConstructorPage />
+        <>
+          <Routes location={background || location}>
+            <Route path='/' element={<ConstructorPage />} />
+            <Route path='/feed' element={<Feed />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/forgot-password' element={<ForgotPassword />} />
+            <Route path='/reset-password' element={<ResetPassword />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/profile/orders' element={<ProfileOrders />} />
+            <Route path='*' element={<NotFound404 />} />
+          </Routes>
+
+          {background && (
+            <Routes>
+              <Route
+                path='/feed/:number'
+                element={
+                  <Modal title='' onClose={handleModalClose}>
+                    <OrderInfo />
+                  </Modal>
+                }
+              />
+              <Route
+                path='/ingredients/:id'
+                element={
+                  <Modal title='Детали ингредиента' onClose={handleModalClose}>
+                    <IngredientDetails />
+                  </Modal>
+                }
+              />
+              <Route
+                path='/profile/orders/:number'
+                element={
+                  <Modal title='' onClose={handleModalClose}>
+                    <OrderInfo />
+                  </Modal>
+                }
+              />
+            </Routes>
+          )}
+        </>
       ) : (
         <div className={`${styles.title} text text_type_main-medium pt-4`}>
-          Нет игредиентов
+          Нет ингредиентов
         </div>
       )}
     </div>
